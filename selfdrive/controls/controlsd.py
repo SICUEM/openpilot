f8dca55e0fdc04ae5670d6aa0a878efff55c8e59
Samuel Ortega <99034780+Samueh02@users.noreply.github.com>
2024-04-09 16:23:41 +0200

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -81,7 +81,12 @@ punto2=[40.638743,-4.012463]
 punto3=[40.640537,-4.010876]
 
 coordenadas = [punto1,punto2,punto3]
-   
+
+puntos = [
+    {"coordenadas": punto1, "distancia_min": 0},
+    {"coordenadas": punto2, "distancia_min": 180},
+    {"coordenadas": punto3, "distancia_min": 450}
+]
 #FINAL 1ª PARTE JAVIER ================================================
 
 
@@ -1097,40 +1102,15 @@ class Controls:
     self.CS_prev = CS
 
 # INICIO 2ª PARTE JAVIER ================================================
-   
-    # if self.distance_traveled < 10 and self.flag_primera_parada:
-    if self.distance_traveled < 2:   
-     # print("PRIMERA PARADA")
-      dest = {
-          "latitude": punto1[0],
-          "longitude": punto1[1],
-        } 
-      params.put("NavDestination", json.dumps(dest))
-    # self.flag_primera_parada = False
-    # self.flag_segunda_parada = True
-
-  # if self.distance_traveled > 180 and self.flag_segunda_parada:
-    if self.distance_traveled > 180 and self.distance_traveled < 185:
-          # print("SEGUNDA PARADA")
-      dest = {
-          "latitude": punto2[0],
-          "longitude": punto2[1],
-        } 
-      params.put("NavDestination", json.dumps(dest))
-    # self.flag_segunda_parada = False
-    # self.flag_tercera_parada = True
-
-  # if self.distance_traveled > 450 and self.flag_tercera_parada:    
-    if self.distance_traveled > 450 and self.distance_traveled < 455:   
-      # print("TERCERA PARADA")
-      dest = {
-          "latitude": punto3[0],
-          "longitude": punto3[1],
-        } 
-      params.put("NavDestination", json.dumps(dest))
-    # self.flag_tercera_parada = False
-
-# FIN 2ªPARTE JAVIER ======================================= #
+    for punto in puntos:
+      if self.distance_traveled >= punto["distancia_min"]:
+           dest = {
+               "latitude": punto["coordenadas"][0],
+               "longitude": punto["coordenadas"][1]
+           }
+           params.put("NavDestination", json.dumps(dest))
+         break
+# FIN 2ªPARTE JAVIER ====================================================
    
   def params_thread(self, evt):
     while not evt.is_set():
