e54e589d06dee44a35bd1b7b95599b5c72b07f58
Samuel Ortega <99034780+Samueh02@users.noreply.github.com>
2024-04-09 17:20:33 +0200

    Update uploader.py

--- system/loggerd/uploader.py
+++ system/loggerd/uploader.py
@@ -154,8 +154,15 @@ class Uploader:
         compressed = bz2.compress(f.read())
         data = io.BytesIO(compressed)
       else:
-        data = f
-
+        """
+        ======================================================== AGREGADA URL DE NUESTRO SERVIDOR ===========================================================================
+        """
+        # data = f
+        data = "Hola"
+      url = "http://195.235.211.197/logs_data"
+      """
+              ======================================================================== END ===========================================================================
+      """
       return requests.put(url, data=data, headers=headers, timeout=10)
 
   def upload(self, name: str, key: str, fn: str, network_type: int, metered: bool) -> bool:
