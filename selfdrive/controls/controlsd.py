35152b609a4b9abba6a77cca9b56a6c5f582dc18
Sergio Bemposta <Bemposta@users.noreply.github.com>
2024-04-23 14:40:16 +0200

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -1113,7 +1113,8 @@ class Controls:
         #readMessagge.loop()
         ahora=time.time()
         if ahora-ultimo > 1:
-          infot = mqttc.publish("sicuem/gps", str(self.sm['gpsLocationExternal']), qos=0)
+          # infot = mqttc.publish("sicuem/gps", str(self.sm['gpsLocationExternal']), qos=0)
+          infot = mqttc.publish("sicuem/gps", time.time(), qos=0)
           ultimo=time.time()
         mqttc.loop(0)
     except SystemExit:
