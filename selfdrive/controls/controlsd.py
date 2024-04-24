2287e3e1db00537c6431ab253eb90aff208f1dc4
Sergio Bemposta <Bemposta@users.noreply.github.com>
2024-04-24 11:02:01 +0200

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -1091,7 +1091,7 @@ class Controls:
   def controlsd_thread(self):
     tpc = TopicMqtt()
     #tpc.ping()
-    tpc.setCanalControlsd(sm)
+    tpc.setCanalControlsd(self.sm)
 
     e = threading.Event()
     t = threading.Thread(target=self.params_thread, args=(e, ))
