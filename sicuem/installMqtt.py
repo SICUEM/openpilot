d7f85bc2fbfac0e46713e6e66f003205350c5194
Sergio Bemposta <Bemposta@users.noreply.github.com>
2024-05-24 12:49:34 +0200

    install Reboot

---
+++ sicuem/installMqtt.py
@@ -0,0 +1,44 @@
+import subprocess
+import sys
+import time
+
+class InstallMqtt:
+
+  def __init__(self):
+    self.instalado = True
+    try:
+      import paho.mqtt.client
+    except ImportError:
+      self.instalado = False
+    return
+
+  def install(self) -> bool:
+    if not self.instalado:
+      try:
+        import paho.mqtt.client
+        self.instalado = True
+      except ImportError:
+        self.instalado = False
+    if not self.instalado:
+      try:
+        code = subprocess.call([sys.executable, "-m", "pip", "install", "paho-mqtt"])
+      except subprocess.CalledProcessError as e:
+        None
+      try:
+        import paho.mqtt.client
+        self.instalado = True
+      except ImportError:
+        self.instalado = False
+    return self.instalado
+
+if __name__ == '__main__':
+  installMqtt = InstallMqtt()
+  if installMqtt.instalado:
+    exit(0)
+  for i in range(10):
+    code = installMqtt.install()
+    if code == True:
+      subprocess.Popen(["systemctl", "reboot", "-i"], shell=True)
+      time.sleep(5)
+      import Javi.Reset.Comma
+    time.sleep(2)
--- sicuem/topicmqtt.py
+++ sicuem/topicmqtt.py
@@ -3,46 +3,48 @@
 
 import time
 import json
-
 from datetime import datetime
-
-paho_instaled = True
 import subprocess
 import sys
 try:
-  import paho.mqtt.client
+  import paho.mqtt.client as mqtt
+  paho_instaled = True
 except ImportError:
-  print("Please install paho-mqtt")
+  code = subprocess.Popen([sys.executable, "/data/openpilot/sicuem/installMqtt.py"])
   paho_instaled = False
 
 class TopicMqtt:
 
   def __init__(self):
     self.fileDebug = "mqttDebug.txt"
+    fileJson = "/data/openpilot/sicuem/canales.json"
     #fileJson = "canales.json"
-    fileJson = "../controls/canales.json"
-    self.indice_canal = 0
-    self.conectado = False
     self.espera = 0.5
+    self.indice_canal = 0
+    self.conetado = False
     self.ultimo = time.time()
-    self.PahoInstaled = paho_instaled
-    self.mqttc = 0
+    self.pahoInstaled = paho_instaled
 
+    if not self.pahoInstaled:
+      return
     with open(fileJson, 'r') as f:
       jsondata = json.load(f)
 
     # Filtrar elementos con enable igual a 1
     self.enabled_items = [item for item in jsondata['canales'] if item['enable'] == 1]
     # Buscar Elemento Speed.
-    speed_value = jsondata['config']['speed']['value']
-    self.espera = 1.0 / float(speed_value)
+    speed_value = None
+    for item in jsondata['config']:
+      if 'speed' in item:
+        speed_value = item['speed']
+        break
+    if speed_value is not None:
+      self.espera = 1.0 / int(speed_value)
 
-  def conectar(self):
     try:
       broker_address = "195.235.211.197"
       #broker_address = "mqtt.eclipseprojects.io"
-      import paho.mqtt.client
-      self.mqttc = paho.mqtt.client.Client(paho.mqtt.client.CallbackAPIVersion.VERSION2)
+      self.mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
       self.mqttc.on_connect = self.on_connect
       self.mqttc.on_disconnect = self.on_disconnect
       self.mqttc.on_subscribe = self.on_subscribe
@@ -50,7 +52,6 @@ class TopicMqtt:
       self.mqttc.subscribe("telemetry_config/speed", 0)
       self.mqttc.on_message = self.on_message
       self.mqttc.loop_start()
-      self.conectado = True
     except Exception as e:
       print("Error en la conexion con el broker mqtt")
       print(e)
@@ -61,41 +62,26 @@ class TopicMqtt:
     f.write(f"[{sttime}] ping...\n")
     f.close()
 
-  def installPaho(self):
-    ahora = time.time()
-    if ahora - self.ultimo > 10:  # Espera 10 sec.
-      self.PahoInstaled = True
-      try:
-        import paho.mqtt.client
-      except ImportError:
-        self.PahoInstaled = False
-      if not self.PahoInstaled:
-        try:
-          subprocess.check_call([sys.executable, "-m", "pip", "install", "paho-mqtt"])
-        except subprocess.CalledProcessError as e:
-          print(e.returncode, e.output)
-      self.ultimo = time.time()
-
   def setCanalControlsd(self, sn):
     self.sm = sn
 
   def on_connect(self, mqttc, obj, flags, reason_code, properties):
     if reason_code == 0:
-      self.conectado = True
+      self.conetado = True
       sttime = datetime.now().strftime('%Y/%m/%d_%H:%M:%S')
       f = open(self.fileDebug, "a")
       f.write(f"[{sttime}] on_connect: {reason_code}\n")
       f.close()
 
   def on_disconnect(self, client, userdata, disconnect_flags, reason_code, properties):
-    self.conectado = False
+    self.conetado = False
     sttime = datetime.now().strftime('%Y/%m/%d_%H:%M:%S')
     f = open(self.fileDebug, "a")
     f.write(f"[{sttime}] on_disconnect: {reason_code}\n")
     f.close()
 
   def on_message(self, mqttc, obj, msg):
-    self.espera = 1.0 / float(msg.payload.decode())
+    self.espera = 1.0 / int(msg.payload.decode())
     sttime = datetime.now().strftime('%Y/%m/%d_%H:%M:%S')
     f = open(self.fileDebug, "a")
     f.write(f"[{sttime}] on_message -> {msg.topic}:{msg.payload.decode()}, value: {self.espera}\n")
@@ -108,16 +94,13 @@ class TopicMqtt:
     f.close()
 
   def loop(self):
-    if not self.PahoInstaled:
-      self.installPaho()
-      return
-    if not self.conectado:
-      self.conectar()
+    if not self.pahoInstaled:
       return
     ahora = time.time()
     if ahora - self.ultimo > self.espera:  # Espera variable.
       canal_actual = self.enabled_items[self.indice_canal]
       self.mqttc.publish(canal_actual['topic'], str(self.sm[canal_actual['canal']]), qos=0)
+      #self.mqttc.publish(canal_actual['topic'], inventa(canal_actual['canal']), qos=0)
       self.indice_canal = (self.indice_canal + 1) % len(self.enabled_items)
       self.ultimo = time.time()
     # self.mqttc.loop(0)
