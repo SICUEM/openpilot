6b442695447f530f25141e35c4ac816c97ec5f85
Sergio Bemposta <Bemposta@users.noreply.github.com>
2024-05-29 10:13:24 +0200

    Update launch_chffrplus.sh

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -90,8 +90,8 @@ function launch {
   # [Start Bemposta] ****************************************************************************
   ip a > /data/openpilot/install_mix.txt
   whoami >> /data/openpilot/install_mix.txt
-  sudo /usr/local/pyenv/versions/3.11.4/bin/python3 -m pip install paho-mqtt >> /data/openpilot/install_mix.txt
-  sudo /usr/local/pyenv/versions/3.11.4/bin/python3 -m pip install confluent-kafka >> /data/openpilot/install_mix.txt
+  pip install paho-mqtt >> /data/openpilot/install_mix.txt
+  pip install confluent-kafka >> /data/openpilot/install_mix.txt
   # [End Bemposta] ******************************************************************************
 
   ./sunnylink.py; ./mapd_installer.py; ./manager.py
