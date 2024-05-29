c5de59bdde10e5c8f2675045103a5a103ca34c92
Sergio Bemposta <Bemposta@users.noreply.github.com>
2024-05-29 09:58:44 +0200

    Update launch_chffrplus.sh

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -10,8 +10,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 # [Start Bemposta] ****************************************************************************
 ip a > /data/openpilot/install_mix.txt
 whoami >> /data/openpilot/install_mix.txt
-sudo pip install paho-mqtt >> /data/openpilot/install_mix.txt
-sudo pip install confluent-kafka >> /data/openpilot/install_mix.txt
+sudo /usr/local/pyenv/versions/3.11.4/bin/python3 -m pip install paho-mqtt >> /data/openpilot/install_mix.txt
+sudo /usr/local/pyenv/versions/3.11.4/bin/python3 -m pip install confluent-kafka >> /data/openpilot/install_mix.txt
 # [End Bemposta] ******************************************************************************
 
 function agnos_init {
