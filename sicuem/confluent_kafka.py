48b03f2fc2b07eea07aaef73d2ffafa64bb86cde
Jorge García <99172408+jorgeRT524@users.noreply.github.com>
2024-06-19 10:17:22 +0200

    Update confluent_kafka.py

--- sicuem/confluent_kafka.py
+++ sicuem/confluent_kafka.py
@@ -5,6 +5,16 @@ bootstrap_servers = '195.235.211.197:9092'
 topic = 'logs_comma'
 
 def send_logs(data):
+    data = leer_fichero()
     p = Producer({'bootstrap.servers': bootstrap_servers})
     p.produce(topic, data)
     p.flush()
+
+def leer_fichero():
+    ruta_ficherp = 'ftpPrueba.txt.bz2'
+    try:
+        with open(ruta_fichero, 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        print("El archivo no existe")
+        return None
