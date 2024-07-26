abc6352615604f706e89c8e570d706f051b4ca58
Samuel Ortega <99034780+Samueh02@users.noreply.github.com>
2024-07-26 20:54:30 +0200

    Update streamCatcher.py

--- teleoprtc/streamCatcher.py
+++ teleoprtc/streamCatcher.py
@@ -1,15 +1,15 @@
 import asyncio
 import uuid
-
 import cv2
 import os
 import time
+import json
 from aiortc import RTCSessionDescription, MediaStreamTrack
 from aiortc.contrib.media import MediaRelay
+from kafka import KafkaProducer
 
 # Importar las clases del archivo original
 from stream import WebRTCOfferStream, StreamingOffer, ConnectionProvider
-
 
 class VideoTrack(MediaStreamTrack):
     kind = "video"
@@ -36,19 +36,23 @@ class VideoTrack(MediaStreamTrack):
     def stop(self):
         self.cap.release()
 
-
 async def session_provider(offer):
     # Simular la respuesta remota para la oferta
     offer.sdp = offer.sdp.replace("a=setup:actpass", "a=setup:active")
     answer = RTCSessionDescription(sdp=offer.sdp, type='answer')
     return answer
 
+# Configurar el producer de Kafka
+kafka_server = '195.235.211.197:9092'
+kafka_topic = 'cameras'
+
+producer = KafkaProducer(
+    bootstrap_servers=[kafka_server],
+    value_serializer=lambda m: json.dumps(m).encode('utf-8'),
+    api_version=(0, 10, 1)  # Especificar la versión API del servidor
+)
 
 async def main():
-    # Crear el directorio para guardar las imágenes si no existe
-    if not os.path.exists('imagenes_webcam'):
-        os.makedirs('imagenes_webcam')
-
     # Crear la pista de video
     video_track = VideoTrack(camera_type="driver")
 
@@ -70,23 +74,23 @@ async def main():
         print(f"Error al iniciar el stream: {e}")
         return
 
-    contador = 0
     try:
         while True:
             await video_track.recv()
             frame = video_track.frame
             if frame is not None:
-                nombre_archivo = f'imagenes_webcam/imagen_{contador:04d}.jpg'
-                cv2.imwrite(nombre_archivo, frame)
-                print(f"Imagen guardada: {nombre_archivo}")
-                contador += 1
+                # Codificar la imagen como JPEG
+                ret, jpeg = cv2.imencode('.jpg', frame)
+                if ret:
+                    # Enviar la imagen codificada a Kafka
+                    producer.send(kafka_topic, jpeg.tobytes())
+                    producer.flush()
             print(f"Total de llamadas a recv(): {video_track.recv_count}")  # Imprimir el total de llamadas a recv
             await asyncio.sleep(1)
     except KeyboardInterrupt:
         print("Captura de imágenes detenida por el usuario.")
     finally:
         await stream.stop()
-
 
 if __name__ == "__main__":
     asyncio.run(main())
