1fcbe2778b98070886ebedde95d40f299eed15d1
Dragoadri <73708124+Dragoadri@users.noreply.github.com>
2025-07-16 11:36:00 +0200

    Update desire_helper.py
    cruise speed

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -158,7 +158,7 @@ class DesireHelper:
   def auto_overtake_without_bsm(self, carstate, d_rel, v_rel, lead_status):
     try:
       if lead_status:
-        velocidad_ok = (carstate.cruiseSpeed - carstate.vEgo) > 4.166  # 15 km/h en m/s
+        velocidad_ok = (carstate.cruiseState.speed - carstate.vEgo) > 4.166  # 15 km/h en m/s
         distancia_ok = d_rel < 50.0
 
         if velocidad_ok and distancia_ok:
@@ -194,6 +194,8 @@ class DesireHelper:
     except Exception as e:
       d_rel, v_rel, lead_status = 0.0, 0.0, False  # valores por defecto seguros
       print(f"❌xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx No se pudo leer lead_info.json: {e}")
+
+    print("****************************** Datosss:", carstate.cruiseState.speed, carstate.vEgo)
 
     if desire_override is not None:
       self.desire = desire_override
