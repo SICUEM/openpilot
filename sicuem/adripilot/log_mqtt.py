a49abf976b813a95f26e99359e01c26dcb969d08
Sergio Bemposta <Bemposta@users.noreply.github.com>
2025-10-14 01:38:58 +0200

    Update log_mqtt.py

--- sicuem/adripilot/log_mqtt.py
+++ sicuem/adripilot/log_mqtt.py
@@ -1,7 +1,7 @@
 # log_mqtt.py
 import json
 from datetime import datetime
-import paho.mqtt.publish as publish
+#import paho.mqtt.publish as publish
 from openpilot.common.params import Params
 import os
 
@@ -21,6 +21,7 @@ DONGLE_ID = params.get("DongleId").decode("utf-8") if params.get("DongleId") els
 
 def enviar_log(mensaje, nivel="INFO", origen="desconocido"):
     topic = f"telemetry_mqtt/{DONGLE_ID}/logs"
+    '''
     payload = {
         "log": mensaje,
         "level": nivel,
@@ -28,9 +29,11 @@ def enviar_log(mensaje, nivel="INFO", origen="desconocido"):
         "origen": origen
     }
     #publish.single(topic, json.dumps(payload), hostname=BROKER, port=PORT)
-
+    '''
+  
 def enviar_log_test(dongle_id_manual, mensaje, nivel="INFO", origen="desconocido"):
     topic = f"telemetry_mqtt/{dongle_id_manual}/logs"
+    '''
     payload = {
         "log": mensaje,
         "level": nivel,
@@ -38,6 +41,7 @@ def enviar_log_test(dongle_id_manual, mensaje, nivel="INFO", origen="desconocido
         "origen": origen
     }
     #publish.single(topic, json.dumps(payload), hostname=BROKER, port=PORT)
+    '''
     print(f"📤 Log de test enviado a {topic}")
 
 
