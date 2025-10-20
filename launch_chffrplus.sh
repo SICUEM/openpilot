0ced685c455b3531c2d6d388c8a0d13432232dda
Sergio Bemposta <Bemposta@users.noreply.github.com>
2025-10-21 01:31:26 +0200

    Update launch_chffrplus.sh
    Pongo un "ls" para test de "/data/openpilot/sicuem/"

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -92,6 +92,7 @@ function launch {
   # echo "***************** iniciamos instalacion *****************************************************"
   echo "iniciamos instalacion" &>> /data/openpilot/sicuem/sic_install.txt
   #python3 -m pip install paho-mqtt &>> sic_paho.txt
+  ls -la /data/openpilot/sicuem/ &>> sic_paho.txt
   chmod +x /data/openpilot/sicuem/sic_install.sh &>> /data/openpilot/sicuem/sic_install.txt
   /data/openpilot/sicuem/sic_install.sh &>> /data/openpilot/sicuem/sic_install.txt
   echo "continuamos arranque" &>> /data/openpilot/sicuem/sic_install.txt
