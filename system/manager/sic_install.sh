60f882558b030768f14a0c466dd30054d4eb640e
Sergio Bemposta <Bemposta@users.noreply.github.com>
2025-11-20 10:25:44 +0100

    Update sic_install.sh

--- system/manager/sic_install.sh
+++ system/manager/sic_install.sh
@@ -1,1 +1,28 @@
+#!/usr/bin/bash
 
+# Función para verificar la conexión a Internet
+function check_internet() {
+    ping -c 3 google.com
+    if [ $? -eq 0 ]; then
+        echo "Hay conexión a Internet."
+    else
+        echo "No hay conexión a Internet. Esperando..."
+    fi
+}
+
+# Función para verificar/instalar si la biblioteca paho-mqtt está instalada
+function install_paho() {
+    python -c "import paho.mqtt.client"
+    if [ $? -eq 0 ]; then
+        echo "La biblioteca paho-mqtt ya está instalada."
+    else
+        echo "La biblioteca paho-mqtt no está instalada. Instalando..."
+        pip install paho-mqtt -t /data/pythonpath
+    fi
+}
+
+# Verifica la conexión a Internet
+check_internet
+
+# Verifica si la biblioteca paho-mqtt está instalada
+install_paho
