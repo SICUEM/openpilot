dfc8db16c36dd88cd1806afe409a066492ff289a
bemposta <sergio.bemposta@gmail.com>
2025-11-20 23:26:40 +0100

    sin de mqtt.

--- sicuem/adripilot/log_mqtt.py
+++ sicuem/adripilot/log_mqtt.py
@@ -84,10 +84,10 @@ params = Params()
 DONGLE_ID = params.get("DongleId").decode("utf-8") if params.get("DongleId") else "UnregisteredDevice"
 
 def enviar_log(mensaje, nivel="INFO", origen="desconocido"):
-    if not test_and_set_paho_async():
-        return
     a = 1
     '''
+    if not test_and_set_paho_async():
+        return
     topic = f"telemetry_mqtt/{DONGLE_ID}/logs"
     payload = {
         "log": mensaje,
@@ -99,10 +99,10 @@ def enviar_log(mensaje, nivel="INFO", origen="desconocido"):
     '''
 
 def enviar_log_test(dongle_id_manual, mensaje, nivel="INFO", origen="desconocido"):
-    if not test_and_set_paho_async():
-        return
     a = 1
     '''
+    if not test_and_set_paho_async():
+        return
     topic = f"telemetry_mqtt/{dongle_id_manual}/logs"
     payload = {
         "log": mensaje,
