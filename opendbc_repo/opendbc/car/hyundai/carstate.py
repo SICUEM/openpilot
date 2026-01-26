cd6f61fe19e0e15142a1e9141c88c17a03428520
Javier Fernandez <javier.fernandez@universidadeuropea.es>
2026-01-26 10:10:14 +0100

    Update carstate.py BSM for Hyundai Tucson HEV 2022 EUR

--- opendbc_repo/opendbc/car/hyundai/carstate.py
+++ opendbc_repo/opendbc/car/hyundai/carstate.py
@@ -183,8 +183,12 @@ class CarState(CarStateBase, EsccCarStateBase, MadsCarState, CarStateExt):
       ret.stockAeb = aeb_warning and aeb_braking
 
     if self.CP.enableBsm:
-      ret.leftBlindspot = cp.vl["LCA11"]["CF_Lca_IndLeft"] != 0
-      ret.rightBlindspot = cp.vl["LCA11"]["CF_Lca_IndRight"] != 0
+      if self.CP.carFingerprint == CAR.HYUNDAI_TUCSON_4TH_GEN:
+        ret.leftBlindspot = cp.vl["BLINDSPOTS_REAR_CORNERS"]["LEFT_MB"] != 0
+        ret.rightBlindspot = cp.vl["BLINDSPOTS_REAR_CORNERS"]["MORE_LEFT_PROB"] != 0
+      else:
+        ret.leftBlindspot = cp.vl["LCA11"]["CF_Lca_IndLeft"] != 0
+        ret.rightBlindspot = cp.vl["LCA11"]["CF_Lca_IndRight"] != 0
 
     # save the entire LKAS11 and CLU11
     self.lkas11 = copy.copy(cp_cam.vl["LKAS11"])
