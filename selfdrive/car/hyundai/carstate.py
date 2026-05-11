ed44099569b9ef75e29f55d2d88361bcfb657d8b
Javier Fernandez <javier.fernandez@universidadeuropea.es>
2026-05-11 15:03:27 +0200

    Quitado el tiempo de retraso desde que se desactiva el BSM del coche 100 = 1 seg, 0 = 0 seg

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -68,8 +68,9 @@ class CarState(CarStateBase):
     # carstate.update() se llama a 100Hz (DT_CTRL=0.01), asi que 200 frames = 2 segundos
     self._left_bsm_counter = 0
     self._right_bsm_counter = 0
-    self.BSM_PERSISTENCE_FRAMES = 100
-
+    # quitado el tiempo de retraso desde que se desactiva el BSM del coche 100 = 1 seg, 0 = 0 seg
+    self.BSM_PERSISTENCE_FRAMES = 0 
+    
   def get_main_enabled(self, ret) -> bool:
     if self.prev_main_buttons != 1 and self.main_buttons[-1] == 1:
       self.mainEnabled = not self.mainEnabled
